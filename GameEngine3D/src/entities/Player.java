5e3819f4a1f6128565bcf21a03faad4f74a367f1
mike <mike@DESKTOP-LD9AJBI.attlocal.net>
2019-02-08 16:43:30 -0600

    Implemented strafing

--- GameEngine3D/src/entities/Player.java
+++ GameEngine3D/src/entities/Player.java
@@ -18,6 +18,7 @@ public class Player extends Entity{
 	private float upwardSpeed = 0;
 	private float currentSpeed = 0;
 	private float currentTurnSpeed = 0;
+	private float currentStrafeSpeed = 0;
 	
 	public Player(int ID, boolean isPlayer, TexturedModel model, Vector3f position, float rotX, float rotY, float rotZ, float scale) {
 		super(ID, isPlayer, model, position, rotX, rotY, rotZ, scale);
@@ -33,10 +34,17 @@ public class Player extends Entity{
 		super.increaseRotation(0, 
 				currentTurnSpeed * DisplayManager.getFrameTimeSeconds(), 0);
 		float distance = currentSpeed * DisplayManager.getFrameTimeSeconds();
+		float strafeDistance = currentStrafeSpeed * DisplayManager.getFrameTimeSeconds();
+
 		float dx = (float) (distance * Math.sin(Math.toRadians(super.getRotY())));
 		float dz = (float) (distance * Math.cos(Math.toRadians(super.getRotY())));
+		
+		float sx = (float) (strafeDistance * Math.cos(Math.toRadians(super.getRotY())));
+		float sz = (float) (strafeDistance * Math.sin(Math.toRadians(super.getRotY())));
+		
 		super.increasePosition(dx, 0, dz);
-
+		super.increasePosition(sx, 0, -sz);
+		
 		upwardSpeed += GRAVITY * DisplayManager.getFrameTimeSeconds();
 		super.increasePosition(0, upwardSpeed * DisplayManager.getFrameTimeSeconds(), 0);
 		
@@ -57,6 +65,14 @@ public class Player extends Entity{
 			this.currentSpeed = -RUN_SPEED;
 		} else {
 			this.currentSpeed = 0;
+		}
+		
+		if (Keyboard.isKeyDown(Keyboard.KEY_Q)) {
+			this.currentStrafeSpeed = RUN_SPEED;
+		} else if (Keyboard.isKeyDown(Keyboard.KEY_E)){
+			this.currentStrafeSpeed = -RUN_SPEED;
+		} else {
+			this.currentStrafeSpeed = 0;
 		}
 		
 		if (Keyboard.isKeyDown(Keyboard.KEY_D)) {
