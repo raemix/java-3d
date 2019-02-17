00565fa471998c959b301883c2a3b4d33c535cd4
raemix <mrmixon82@gmail.com>
2019-02-16 19:08:41 -0600

    Added support for mulitple lights

--- GameEngine3D/src/engineTester/MainGameLoop.java
+++ GameEngine3D/src/engineTester/MainGameLoop.java
@@ -147,8 +147,13 @@ public class MainGameLoop {
         }
         
          
-        Light light = new Light(new Vector3f(20000,20000,2000),
+        Light light = new Light(new Vector3f(0,10000,-7000),
         		new Vector3f(1,1,1));
+        List<Light> lights = new ArrayList<Light>();
+        lights.add(light);
+        lights.add(new Light(new Vector3f(-200,10,-200), new Vector3f(10,0,0)));
+        lights.add(new Light(new Vector3f(200,10,200), new Vector3f(0,0,10)));
+
 
         MasterRenderer renderer = new MasterRenderer();
         
@@ -191,7 +196,7 @@ public class MainGameLoop {
                 renderer.processEntity(entity);
             	}
             }
-            renderer.render(light, camera);
+            renderer.render(lights, camera);
             guiRenderer.render(guis);
             DisplayManager.updateDisplay();
         }
